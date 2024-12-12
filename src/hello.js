7f2e8656d048b1c9c79fa9bb1dfde8ea4296b1c0
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-12 19:18:34 +0600

    mychat11

---
+++ src/hello.js
@@ -0,0 +1,15 @@
+// Create a script element
+(function () {
+    const script = document.createElement("script");
+  
+    // Set the src attribute to the provided URL
+    script.src = "https://chat-ui-dj7x.vercel.app/";
+  
+    // Optionally, you can set the type and async attribute
+    script.type = "text/javascript";
+    script.async = true;
+  
+    // Append the script tag to the head (or body) of the document
+    document.head.appendChild(script);
+  });
+  
