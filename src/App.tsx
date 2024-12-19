9bcab2fb57492bb572023dc6f107e1f67aa7188d
Israr Munim Chowdhury <israr.munim@bjitgroup.com>
2024-12-19 22:19:45 +0600

    another design

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      <iframe
-        width="100"
-        height="100"
-        style={{
-          border: 'none',
-          position: 'relative',
-          top: '-319px',
-          right: '14px',
-        }}
-      ></iframe>
-
-      <div
-        style={{
-          width: '4rem',
-          height: '4rem',
-          backgroundColor: '#f56565',
-          position: 'absolute',
-          top: '0',
-        }}
-      >
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M17 12C17 12.9889 16.7068 13.9556 16.1573 14.7778C15.6079 15.6001 14.827 16.241 13.9134 16.6194C12.9998 16.9978 11.9945 17.0969 11.0245 16.9039C10.0546 16.711 9.16373 16.2348 8.46447 15.5355C7.76521 14.8363 7.289 13.9454 7.09608 12.9754C6.90315 12.0055 7.00217 11.0002 7.3806 10.0866C7.75904 9.17295 8.3999 8.39206 9.22215 7.84265C10.0444 7.29324 11.0111 7 12 7C13.4 7 14.7389 7.55556 15.7444 8.52222L17 9.77778"
-            stroke="white"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M16.9999 7V9.77778H14.2222"
-            stroke="white"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </div>
       <div
         style={{
           position: 'fixed',
-          bottom: '0',
+          bottom: '78px',
           right: '20px',
           width: '400px',
           height: '600px',
@@ -103,6 +60,38 @@ function App() {
             I need help with my account.
           </div>
         </div>
+      </div>
+      <div
+        style={{
+          width: '4rem',
+          height: '4rem',
+          backgroundColor: '#f56565',
+          position: 'absolute',
+          bottom: '0',
+        }}
+      >
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M17 12C17 12.9889 16.7068 13.9556 16.1573 14.7778C15.6079 15.6001 14.827 16.241 13.9134 16.6194C12.9998 16.9978 11.9945 17.0969 11.0245 16.9039C10.0546 16.711 9.16373 16.2348 8.46447 15.5355C7.76521 14.8363 7.289 13.9454 7.09608 12.9754C6.90315 12.0055 7.00217 11.0002 7.3806 10.0866C7.75904 9.17295 8.3999 8.39206 9.22215 7.84265C10.0444 7.29324 11.0111 7 12 7C13.4 7 14.7389 7.55556 15.7444 8.52222L17 9.77778"
+            stroke="white"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M16.9999 7V9.77778H14.2222"
+            stroke="white"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
       </div>
     </>
   )
